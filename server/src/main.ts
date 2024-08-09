21046bd535d86d203a27df6fe2ca5b78044ff1a1
deepansh <mauryadeepansh40@gmail.com>
2024-08-09 19:20:17 +0530

    done

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import * as Websocket from "ws";
 import { websocketConnection } from "./lib/ws";
+dotenv.config();
 const main = async () => {
   const app = express();
   const port = process.env.PORT;
+  console.log(port);
+
   const server = await app.listen(port, () => {
     console.log("main server started");
   });
